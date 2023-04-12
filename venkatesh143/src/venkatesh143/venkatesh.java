9ff8b16ae556833028e15e3ab1a44b04ce640e7e
P VENKATESH <P VENKATESH@DESKTOP-420506K.bbrouter>
2023-04-12 21:39:15 +0530

    Pushing Venkatesh143 to Git Rep

---
+++ venkatesh143/src/venkatesh143/venkatesh.java
@@ -0,0 +1,13 @@
+package venkatesh143;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class venkatesh {
+
+	public static void main(String[] args) {
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+
+	}
+
+}
